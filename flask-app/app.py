fa5ce46abb29f2ec4d3a005e48f7b2988c1318b7
David <davidnikishov1324@gmail.com>
2026-01-09 01:09:03 +0200

    3 diff endpoint

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,3 +2,39 @@ from flask import Flask, jsonify
 import time
 import random
 
+app = Flask(__name__)
+
+@app.route("/fast")
+def fast():
+    return jsonify(
+        endpoint="fast",
+        status="ok"
+    ), 200
+
+
+@app.route("/slow")
+def slow():
+    time.sleep(1.5)  # имитация долгой операции
+    return jsonify(
+        endpoint="slow",
+        delay="1.5s",
+        status="ok"
+    ), 200
+
+
+@app.route("/unstable")
+def unstable():
+    if random.random() < 0.3:  # ~30% ошибок
+        return jsonify(
+            endpoint="unstable",
+            status="error"
+        ), 500
+
+    return jsonify(
+        endpoint="unstable",
+        status="ok"
+    ), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
